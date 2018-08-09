171dccee3c26ba711d68965fb259d2b0ecb262df
PiroHiroPiro <hirohiro11071107@gmail.com>
2018-08-10 00:54:02 +0900

    change to pocket url from article url

--- pickup/src/lambda_function.py
+++ pickup/src/lambda_function.py
@@ -6,7 +6,6 @@ import requests
 import os
 import logging
 import random
-from urllib.parse import parse_qs
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -16,6 +15,7 @@ SLACK_POST_URL = os.environ['SLACK_POST_URL']
 SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
 POCKET_CONSUMER_KEY = os.environ['POCKET_CONSUMER_KEY']
 POCKET_ACCESS_TOKEN = os.environ['POCKET_ACCESS_TOKEN']
+POCKET_ARTICLE_BASE_URL = 'https://getpocket.com/a/read/%s'
 
 POCKET_GET_API_URL = "https://getpocket.com/v3/get"
 HEADERS = {
@@ -23,6 +23,7 @@ HEADERS = {
     'x-accept': "application/json",
     'cache-control': "no-cache"
 }
+
 
 def pick_up():
     try:
@@ -42,21 +43,15 @@ def pick_up():
         res_json = res.json()
         items = list(res_json['list'].keys())
         item_id = random.choice(items)
-        text = "%s (%s)" % (res_json['list'][item_id]['resolved_title'], res_json['list'][item_id]['resolved_url'])
+        text = "%s (%s)" % (res_json['list'][item_id]['resolved_title'], POCKET_ARTICLE_BASE_URL % res_json['list'][item_id]['item_id'])
         color = "good"
     except:
         text = "Failed!"
         color = "#ff0000"
-    return { "text": text, "color": color }
+    return {"text": text, "color": color}
+
 
 def lambda_handler(event, context):
-    token = os.environ['SLACK_OUTGOING_WEBHOOK_TOKEN']
-    # query = parse_qs(event.get('body') or '')
-    # if query.get('token', [''])[0] != token:
-    #     return { 'statusCode': 400 }
-    # slackbot_name = 'pocketer'
-    # if query.get('user_name', [slackbot_name])[0] == slackbot_name:
-    #     return { 'statusCode': 200 }
     content = pick_up()
     slack_message = {
         'channel'    : SLACK_CHANNEL,
@@ -67,7 +62,7 @@ def lambda_handler(event, context):
     try:
         req = requests.post(SLACK_POST_URL, data=json.dumps(slack_message))
         logger.info('Message posted to %s', slack_message['channel'])
-        return { 'statusCode': 200 }
+        return {'statusCode': 200}
     except requests.exceptions.RequestException as e:
         logger.error('Request failed: %s', e)
-        return { 'statusCode': 400 }
+        return {'statusCode': 400}
