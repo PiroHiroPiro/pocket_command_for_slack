02886449e034f3eecf8d4254328df693372f7d0e
PiroHiroPiro <hirohiro11071107@gmail.com>
2018-12-30 03:52:12 +0900

    refactoring pickup

--- pickup/src/lambda_function.py
+++ pickup/src/lambda_function.py
@@ -6,65 +6,82 @@ import requests
 import os
 import logging
 import random
+from urllib.parse import parse_qs
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-logging.basicConfig(format='%(asctime)s- %(name)s- %(message)s')
+logging.basicConfig(format="%(asctime)s- %(name)s- %(message)s")
 
-SLACK_POST_URL = os.environ['SLACK_POST_URL']
-SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
-POCKET_CONSUMER_KEY = os.environ['POCKET_CONSUMER_KEY']
-POCKET_ACCESS_TOKEN = os.environ['POCKET_ACCESS_TOKEN']
-POCKET_ARTICLE_BASE_URL = 'https://getpocket.com/a/read/%s'
+SLACK_POST_URL = os.environ["SLACK_POST_URL"]
+SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
 
+POCKET_CONSUMER_KEY = os.environ["POCKET_CONSUMER_KEY"]
+POCKET_ACCESS_TOKEN = os.environ["POCKET_ACCESS_TOKEN"]
+POCKET_ARTICLE_BASE_URL = "https://getpocket.com/a/read/%s"
 POCKET_GET_API_URL = "https://getpocket.com/v3/get"
-HEADERS = {
-    'content-type': "application/json;charset = UTF8",
-    'x-accept': "application/json",
-    'cache-control': "no-cache"
+POCKET_HEADERS = {
+    "content-type": "application/json;charset = UTF8",
+    "x-accept": "application/json",
+    "cache-control": "no-cache"
 }
 
 
-def pick_up():
+def pick_up_item():
     try:
-        payload = {
-            "consumer_key": POCKET_CONSUMER_KEY,
-            "access_token": POCKET_ACCESS_TOKEN,
-            "tag": [
+
+        not_include_tags = [
                 "amazon_dash_button",
                 "conference",
                 "done",
                 "tool",
                 "twitter"
-            ],
+        ]
+        payload = {
+            "consumer_key": POCKET_CONSUMER_KEY,
+            "access_token": POCKET_ACCESS_TOKEN,
+            "tag": not_include_tags,
             "sort": "oldest",
             "count": 2000
         }
-        res = requests.request("POST", POCKET_GET_API_URL, data=json.dumps(payload), headers=HEADERS)
+
+        res = requests.request("POST", POCKET_GET_API_URL, data=json.dumps(payload), headers=POCKET_HEADERS)
         res.raise_for_status()
         res_json = res.json()
-        items = list(res_json['list'].keys())
-        item_id = random.choice(items)
-        text = "%s (%s)" % (res_json['list'][item_id]['resolved_title'], POCKET_ARTICLE_BASE_URL % res_json['list'][item_id]['item_id'])
+
+        item_ids = list(res_json["list"].keys())
+        picked_item_id = random.choice(item_ids)
+
+        picked_item_title = res_json["list"][picked_item_id]["resolved_title"]
+        picked_item_url = POCKET_ARTICLE_BASE_URL % res_json["list"][picked_item_id]["item_id"]
+        text = "%s (%s)" % (picked_item_title, picked_item_url)
         color = "good"
     except:
-        text = "Failed!"
+        text = "Pick up post Failed!"
         color = "#ff0000"
+
     return {"text": text, "color": color}
 
 
 def lambda_handler(event, context):
-    content = pick_up()
+    token = os.environ["SLACK_OUTGOING_WEBHOOK_TOKEN"]
+    query = parse_qs(event.get("body") or "")
+    if query.get("token", [""])[0] != token:
+        logger.error("Undefined token: %s", query.get("token", [""])[0])
+        return { "statusCode": 400 }
+
+    content = pick_up_item()
     slack_message = {
-        'channel'    : SLACK_CHANNEL,
-        'attachments': [
+        "channel"    : SLACK_CHANNEL,
+        "attachments": [
             content
         ],
     }
+
     try:
         req = requests.post(SLACK_POST_URL, data=json.dumps(slack_message))
-        logger.info('Message posted to %s', slack_message['channel'])
-        return {'statusCode': 200}
+        logger.info("Message posted to %s", slack_message["channel"])
+        return {"statusCode": 200}
     except requests.exceptions.RequestException as e:
-        logger.error('Request failed: %s', e)
-        return {'statusCode': 400}
+        logger.error("Request failed: %s", e)
+        return {"statusCode": 400}
