3c93fd546ff6b4e80bda237b49296229bb664158
PiroHiroPiro <hirohiro11071107@gmail.com>
2018-12-30 12:55:19 +0900

    modify select

--- select/src/lambda_function.py
+++ select/src/lambda_function.py
@@ -31,15 +31,19 @@ QIITA_HEADERS = {
     "Authorization": "Bearer 18465239840fcf5bc4be148777637e0ac9439893",
     "cache-control": "no-cache"
 }
-QIITA_HOST = "https://qiita.com"
+QIITA_HOST = "https://qiita.com/"
 QIITA_LIKES_THRESHOLD = 100
 
-def get_likes(item_id):
-    qiita_item_url = QIITA_GET_API_URL + item_id
-    res = requests.request("GET", qiita_item_url, headers=QIITA_HEADERS)
-    res.raise_for_status()
-    res_json = res.json()
-    return res_json["likes_count"]
+
+def get_likes(qiita_item_id):
+    try:
+        qiita_item_url = QIITA_GET_API_URL + qiita_item_id
+        res = requests.request("GET", qiita_item_url, headers=QIITA_HEADERS)
+        res.raise_for_status()
+        res_json = res.json()
+        return res_json["likes_count"]
+    except:
+        return 0
 
 
 def select_qiita():
@@ -58,26 +62,28 @@ def select_qiita():
         actions = []
 
         for item_id in res_json["list"].keys():
-            if "resolved_id" not in res_json["list"][item_id].keys():
+            if "resolved_url" not in res_json["list"][item_id].keys():
                 continue
 
-            if not res_json["list"][item_id]["resolved_id"].startswith(QIITA_HOST):
+            if not res_json["list"][item_id]["resolved_url"].startswith(QIITA_HOST):
                 continue
 
-            qiita_item_id = res_json["list"][item_id]["resolved_id"].split("/")[-1]
-            qiita_item_likes = get_likes(qiita_item_id)
-            if qiita_item_likes >= QIITA_LIKES_THRESHOLD:
-                action = {
-                    "action" : "tags_remove",
-                    "item_id": item_id,
-                    "tags"   : "twitter"
-                }
-                action = {
-                    "action" : "tags_add",
-                    "item_id": item_id,
-                    "tags"   : "selected_qiita"
-                }
-                actions.append(action)
+            qiita_item_id = res_json["list"][item_id]["resolved_url"].split("/")[-1] # 最後の"/"以降の文字列を取得
+            item_likes = get_likes(qiita_item_id)
+            logger.info("item_id %s (qiita_id %s) has %d likes.", item_id, qiita_item_id, item_likes)
+            remove_action = {
+                "action" : "tags_remove",
+                "item_id": item_id,
+                "tags"   : "twitter"
+            }
+            actions.append(remove_action)
+            add_tag = "selected_qiita" if item_likes >= QIITA_LIKES_THRESHOLD else "not_selected_qiita"
+            add_action = {
+                "action" : "tags_add",
+                "item_id": item_id,
+                "tags"   : add_tag
+            }
+            actions.append(add_action)
 
         if len(actions) > 0:
             payload = {
@@ -88,10 +94,10 @@ def select_qiita():
             res = requests.request("POST", POCKET_SEND_API_URL, data=json.dumps(payload), headers=POCKET_HEADERS)
             res.raise_for_status()
 
-        text  = "Success! %d items were selected." % (len(actions) / 2)
+        text  = "Select success! %d items were selected." % (len(actions) / 2)
         color = "good"
     except:
-        text  = "Failed!"
+        text  = "Select failed!"
         color = "#ff0000"
 
     return { "text": text, "color": color }
@@ -101,6 +107,7 @@ def lambda_handler(event, context):
     token = os.environ["SLACK_OUTGOING_WEBHOOK_TOKEN"]
     query = parse_qs(event.get("body") or "")
     if query.get("token", [""])[0] != token:
+        logger.error("Undefined token: %s", query.get("token", [""])[0])
         return { "statusCode": 400 }
 
     content = select_qiita()
