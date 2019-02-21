19a985c65a11faab0df3f8ce65dc1d31ecfbbb10
PiroHiroPiro <hirohiro11071107@gmail.com>
2019-02-21 13:24:46 +0900

    refactoring

--- pickup/src/lambda_function.py
+++ pickup/src/lambda_function.py
@@ -2,11 +2,12 @@
 # encoding: utf-8
 
 import json
-import requests
 import os
 import logging
 import random
 from urllib.parse import parse_qs
+
+import requests
 
 
 logger = logging.getLogger()
@@ -26,72 +27,68 @@ POCKET_HEADERS = {
     "cache-control": "no-cache"
 }
 
-random_255 = lambda: random.randint(0,255)
+NOT_INCLUDE_TAGS = [
+    "amazon_dash_button",
+    "conference",
+    "done",
+    "tool",
+    "twitter"
+]
+
+RANDOM255 = lambda: random.randint(0,255)
 
 
 def pick_up_item():
-    try:
-        not_include_tags = [
-                "amazon_dash_button",
-                "conference",
-                "done",
-                "tool",
-                "twitter"
-        ]
-        payload = {
-            "consumer_key": POCKET_CONSUMER_KEY,
-            "access_token": POCKET_ACCESS_TOKEN,
-            "tag": not_include_tags,
-            "sort": "oldest",
-            "count": 2000
-        }
+    payload = {
+        "consumer_key": POCKET_CONSUMER_KEY,
+        "access_token": POCKET_ACCESS_TOKEN,
+        "tag": NOT_INCLUDE_TAGS,
+        "sort": "oldest",
+        "count": 5000
+    }
 
-        res = requests.request("POST", POCKET_GET_API_URL, data=json.dumps(payload), headers=POCKET_HEADERS)
-        res.raise_for_status()
-        res_json = res.json()
+    res = requests.request("POST", POCKET_GET_API_URL, data=json.dumps(payload), headers=POCKET_HEADERS)
+    res.raise_for_status()
+    res_json = res.json()
 
-        item_ids = list(res_json["list"].keys())
-        item_id = random.choice(item_ids)
+    item_ids = list(res_json["list"].keys())
+    item_id = random.choice(item_ids)
 
-        item_title = res_json["list"][item_id]["resolved_title"]
-        item_url = POCKET_ARTICLE_BASE_URL % res_json["list"][item_id]["item_id"]
-        time_added = res_json["list"][item_id]["time_added"]
-        color_code = "#%02X%02X%02X" % (random_255(),random_255(),random_255())
+    item_title = res_json["list"][item_id]["resolved_title"]
+    item_url = POCKET_ARTICLE_BASE_URL % res_json["list"][item_id]["item_id"]
+    time_added = res_json["list"][item_id]["time_added"]
+    color_code = "#%02X%02X%02X" % (RANDOM255(), RANDOM255(), RANDOM255())
 
-        content = {
-            "fallback": item_title,
-            "color": color_code,
-            "fields":[
-                {
-                "title": item_title,
-                "value": item_url
-                }
-            ],
-            "ts": time_added
-        }
-
-    except:
-        content = {
-            "text": "Pick up Failed!",
-            "color": "danger"
-        }
+    content = {
+        "fallback": item_title,
+        "color": color_code,
+        "fields":[
+            {
+            "title": item_title,
+            "value": item_url
+            }
+        ],
+        "ts": time_added
+    }
 
     return content
 
 
 def lambda_handler(event, context):
-    content = pick_up_item()
-    slack_message = {
-        "channel"    : SLACK_CHANNEL,
-        "attachments": [
-            content
-        ],
-    }
-
     try:
-        req = requests.post(SLACK_POST_URL, data=json.dumps(slack_message))
+        content = pick_up_item()
+
+        slack_message = {
+            "channel"    : SLACK_CHANNEL,
+            "attachments": [
+                content
+            ],
+        }
+
+        res = requests.request("POST", SLACK_POST_URL, data=json.dumps(slack_message))
+        res.raise_for_status()
         logger.info("Message posted to %s", slack_message["channel"])
         return {"statusCode": 200}
-    except requests.exceptions.RequestException as e:
-        logger.error("Request failed: %s", e)
+    except Exception as e:
+        logger.error("Error: %s", e)
         return {"statusCode": 400}
